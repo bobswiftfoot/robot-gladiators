f020234efb19171759ec6fe4dac00386bab1f93f
Nate Pfau <natepfau@yahoo.com>
2021-05-12 18:49:33 -0500

    Random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,7 +21,7 @@ function startGame()
         {
             window.alert("Welcome to ROBOT GLADIATORS! Round: " + (i + 1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
 
             // if we're not at the last enemy in the array
@@ -56,7 +56,7 @@ function fight(enemyName)
             if(confirmSkip)
             {
                 window.alert(playerName + " has chosen to skip the fight!");
-                playerMoney -= 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 break;
             }
             else
@@ -65,8 +65,9 @@ function fight(enemyName)
             }
         }
 
+        var damage = randomNumber(playerAttack - 3, playerAttack);
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-        enemyHealth -= playerAttack;
+        enemyHealth = Math.max(0, enemyHealth - damage);
         // Log a resulting message to the console so we know that it worked.
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
         if (enemyHealth <= 0) 
@@ -78,8 +79,10 @@ function fight(enemyName)
         {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
+
+        damage = randomNumber(enemyAttack - 3, enemyAttack);
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-        playerHealth -= enemyAttack;
+        playerHealth = Math.max(playerHealth - damage);
         // Log a resulting message to the console so we know that it worked.
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
         if (playerHealth <= 0) 
@@ -169,5 +172,13 @@ function shop()
         break;
     }
 }
+
+// function to generate a random numeric value
+function randomNumber(min, max) 
+{
+    var value = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+    return value;
+ };
 
 startGame();
